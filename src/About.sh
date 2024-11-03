e47ca8ed0038aa1645b96ecbe18faf6ea4ca1c1a
Shariar Sakib <glitchprovider@gmail.com>
2024-11-03 14:29:26 +0600

    Update About.sh

--- src/About.sh
+++ src/About.sh
@@ -1,6 +1,6 @@
 echo '
     ┌──────────────────────────────────────────────────────────────┐
-    │  	               ┏━┓┏┓ ┏━┓╻ ╻╺┳╸	              │
+    │  	           ┏━┓┏┓ ┏━┓╻ ╻╺┳╸	          │
     │  		       ┣━┫┣┻┓┃ ┃┃ ┃ ┃ 		      │
     │  		       ╹ ╹┗━┛┗━┛┗━┛ ╹ 		      │
     │──────────────────────────────────────────────────────────────│
@@ -8,10 +8,10 @@ echo '
     │ >>   Hi, here you can see my telegram and github there       |
     |       will be questions you will write in telegram.          │
     │                                                              │
-    │ >>  Author   :  Misha Korzhik                                │
-    │     Github   :  https://github.com/mishakorzik               │
-    │     Telegram :  pseudonym @MishaKorzhikTelegram              │
-    |     Info     :  Trank for installing AllHackingTools         |
+    │ >>  Author   :  SHARIAR SAKIB                                │
+    │     Github   :  https://github.com/Chooperkutt9              │
+    │     Telegram :  Shariar Sakib                                │
+    |     Info     :  Thanks for installing AllHackingTools         |
     │                                                              │
     └──────────────────────────────────────────────────────────────┘ 
 '|lolcat -a -d 10 -p 1.5 -s 40.0
